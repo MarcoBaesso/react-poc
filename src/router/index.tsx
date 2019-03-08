953ff1b607e8bcfe39e67a431d6c399d89c9841f
baessom <marco.baesso@previnet.it>
2019-03-08 12:32:05 +0100

    change tsconfig

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {Router} from 'react-router';
 import { Route, Switch } from 'react-router'
 import { History, createBrowserHistory } from 'history';
-import App from "../app";
+import App from "app";
 
 export const history: History = createBrowserHistory({
     basename: "subdirectory"
@@ -22,7 +22,7 @@ export class AppRouter extends React.PureComponent {
         return (
             <Router history={history}>
                 <Switch>
-                    <Route path="/app" component={App}/>
+                    <Route path="/main" component={App}/>
                     {/*
                     <Route path="/" component={NoMatch}/>
                     */}
